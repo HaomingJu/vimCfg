3e02a5c7ac1053b77e75d3f8a2cf73c5dac78e29
juhaoming@outlook.com <juhaoming@trunk.tech>
2021-08-05 10:52:47 +0800

    重新编译vim, 支持剪切板+clipboard

--- install.sh
+++ install.sh
@@ -1,33 +1,33 @@
 #!/usr/bin/env bash
 
-vim_version="8.2.3249"
+vim_version="8.2.3290"
 auth=sudo
 
-echo "Update apt" >&2
-${auth} apt update 2>&1 > /dev/null
+echo "Update apt"
+${auth} apt update
 
 source /etc/lsb-release
 
 if [ ${DISTRIB_RELEASE} == "16.04" ]; then
-    ${auth} apt install -y ruby2.3-dev 2>&1 > /dev/null
+    ${auth} apt install -y ruby2.3-dev
 fi
 if [ ${DISTRIB_RELEASE} == "18.04" ]; then
-    ${auth} apt install -y ruby2.5-dev 2>&1 > /dev/null
+    ${auth} apt install -y ruby2.5-dev 
 fi
 if [ ${DISTRIB_RELEASE} == "20.04" ]; then
-    ${auth} apt install -y ruby2.7-dev 2>&1 > /dev/null
+    ${auth} apt install -y ruby2.7-dev
 fi
 
 
 echo "START: Install build vim dep debs" >&2
-${auth} apt install -y ctags cscope curl libncurses5-dev libperl-dev python2.7-dev python2.7-dbg 2>&1 > /dev/null
+${auth} apt install -y ctags cscope curl libncurses5-dev libperl-dev python2.7-dev python2.7-dbg
 echo "DONE: Install build vim dep debs" >&2
 
 echo "START: Install npm & nodejs" >&2
-${auth} apt install -y npm 2>&1 > /dev/null
-${auth} npm config set registry https://registry.npm.taobao.org 2>&1 > /dev/null
-${auth} npm install n -g 2>&1 > /dev/null
-${auth} n stable 2>&1 > /dev/null
+${auth} apt install -y npm 
+${auth} npm config set registry https://registry.npm.taobao.org 
+${auth} npm install n -g 
+${auth} n stable 
 echo "DONE: Install npm & nodejs" >&2
 
 echo "START: Download vim source tar.gz" >&2
@@ -35,13 +35,12 @@ curl -o vim-${vim_version}.tar.gz -L https://github.com/vim/vim/archive/refs/tag
 echo "DONE: Download vim source tar.gz" >&2
 
 echo "START: Extract vim source tar.gz" >&2
-tar -zxvf vim-${vim_version}.tar.gz 2>&1 > /dev/null
+tar -zxvf vim-${vim_version}.tar.gz
 echo "DONE: Extract vim source tar.gz" >&2
 
 echo "START: Build vim" >&2
 pushd vim-${vim_version}/
-./configure \ 
-    --disable-libsodium \
+./configure --disable-libsodium \
     --with-features=huge \
     --enable-multibyte \
     --enable-pythoninterp=yes  \
@@ -49,10 +48,11 @@ pushd vim-${vim_version}/
     --with-python-config-dir=`python2.7-config  --configdir`  \
     --enable-rubyinterp=yes \
     --enable-perlinterp=yes \
-    --enable-cscope 2>&1 > /dev/null
+    --enable-cscope \
+    --with-x
 
-make 2>&1 > /dev/null
-${auth} make install 2>&1 > /dev/null
+make 
+${auth} make install 
 popd
 echo "DONE: Build vim" >&2
 
