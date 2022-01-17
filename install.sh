3ada18d1536bfdca5a3ac7ac1f8abbcecce666ba
HaomingJu <juhaoming@trunk.tech>
2022-01-17 12:48:35 +0800

    更新安装脚本, 支持vim和neovim的下载安装
    * vim和neovim均跟随最新代码进行编译

--- install.sh
+++ install.sh
@@ -1,71 +1,77 @@
 #!/usr/bin/env bash
 
-vim_version="8.2.3863"
-auth=sudo
+build_vim=true
+build_neovim=true
 
-#vim_with_python="--enable-pythoninterp=yes  \
-    #--with-python-command=`which python2` \
-    #--with-python-config-dir=`python2.7-config  --configdir` \
-    #"
+vim_with_python2="--enable-pythoninterp=yes  \
+    --with-python-command=`which python2` \
+    --with-python-config-dir=`python2.7-config  --configdir` \
+    "
 
-vim_with_python="--enable-python3interp=yes  \
+vim_with_python3="--enable-python3interp=yes  \
     --with-python3-command=`which python3` \
     --with-python3-config-dir=`python3-config  --configdir` \
     "
+vim_with_python=$vim_with_python3
 
 echo "Update apt"
-${auth} apt update
+sudo apt update
 
 source /etc/lsb-release
 
 if [ ${DISTRIB_RELEASE} == "16.04" ]; then
-    ${auth} apt install -y ruby2.3-dev
+    sudo apt install -y ruby2.3-dev
 fi
 if [ ${DISTRIB_RELEASE} == "18.04" ]; then
-    ${auth} apt install -y ruby2.5-dev 
+    sudo apt install -y ruby2.5-dev 
 fi
 if [ ${DISTRIB_RELEASE} == "20.04" ]; then
-    ${auth} apt install -y ruby2.7-dev
+    sudo apt install -y ruby2.7-dev
 fi
 
 
 echo "START: Install build vim dep debs" >&2
-${auth} apt install -y ctags cscope curl libncurses5-dev libperl-dev vim-scripts vim-gtk vim-gnome vim-common
+sudo apt install -y ctags cscope curl libncurses5-dev libperl-dev vim-scripts vim-gtk vim-gnome vim-common
 echo "DONE: Install build vim dep debs" >&2
 
 echo "START: Install npm & nodejs" >&2
-${auth} apt install -y npm 
-${auth} npm config set registry https://registry.npm.taobao.org 
-${auth} npm install n -g 
-${auth} n latest
-${auto} n prune
+sudo apt install -y npm
+sudo npm config set registry https://registry.npm.taobao.org 
+sudo npm install n -g 
+sudo n latest
+sudo n prune
 echo "DONE: Install npm & nodejs" >&2
 
-echo "START: Download vim source tar.gz" >&2
-curl -o vim-${vim_version}.tar.gz -L https://github.com/vim/vim/archive/refs/tags/v${vim_version}.tar.gz
-echo "DONE: Download vim source tar.gz" >&2
 
-echo "START: Extract vim source tar.gz" >&2
-tar -zxvf vim-${vim_version}.tar.gz
-echo "DONE: Extract vim source tar.gz" >&2
+if $build_vim; then
+    rm -rf ~/.vim/build/vim
+    git clone --depth 1 https://github.com/vim/vim.git ~/.vim/build/vim
+    pushd ~/.vim/vim
+    ./configure --disable-libsodium \
+        --with-features=huge \
+        --enable-multibyte \
+        ${vim_with_python} \
+        --enable-rubyinterp=yes \
+        --enable-perlinterp=yes \
+        --enable-cscope \
+        --with-x
 
-echo "START: Build vim" >&2
-pushd vim-${vim_version}/
-./configure --disable-libsodium \
-    --with-features=huge \
-    --enable-multibyte \
-    ${vim_with_python}
-    --enable-rubyinterp=yes \
-    --enable-perlinterp=yes \
-    --enable-cscope \
-    --with-x
+    make
+    sudo make install
+    popd
+    echo "DONE: Build vim" >&2
+fi
 
-make 
-${auth} make install 
+if $build_neovim; then
+    rm -rf ~/.vim/build/neovim/
+    git clone --depth 1 https://github.com/neovim/neovim.git ~/.vim/build/neovim
+    pushd ~/.vim/build/neovim
+    make CMAKE_BUILD_TYPE=Release
+    sudo make install
 popd
-echo "DONE: Build vim" >&2
 
-vim --version
+echo "DONE: Build neovim"
+fi
+
 vim -c :PlugInstall
-
 # :CocCommand clangd.install 命令要下载的工具连接 https://github.com/clangd/clangd/releases/tag/13.0.0
